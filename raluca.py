9a65c23ae0fb060290a64ddb2dee78656b1d8bd7
Raluca <raluca.nicko@gmail.com>
2024-04-25 01:13:56 +0300

    add fibonacci while version

---
+++ raluca.py
@@ -0,0 +1,11 @@
+print("nada")
+lista=[]
+i=2
+lista.append(1)
+lista.append(1)
+n = int(input("lalala"))
+while i < n:
+    lista.append(lista[i-1]+lista[i-2])
+    print(lista[i])
+    i= i+1
+    
